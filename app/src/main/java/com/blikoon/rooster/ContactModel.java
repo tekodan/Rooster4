54b83d8fcf2d3526fe087608e77c8b6b96d29ba2
root <tekodan@gmail.com>
2017-05-02 09:12:33 -0500

    Initial commit

---
+++ app/src/main/java/com/blikoon/rooster/ContactModel.java
@@ -0,0 +1,46 @@
+package com.blikoon.rooster;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gakwaya on 4/16/2016.
+ */
+public class ContactModel {
+
+    private static ContactModel sContactModel;
+    private List<Contact> mContacts;
+
+    public static ContactModel get(Context context)
+    {
+        if(sContactModel == null)
+        {
+            sContactModel = new ContactModel(context);
+        }
+        return  sContactModel;
+    }
+
+    private ContactModel(Context context)
+    {
+        mContacts = new ArrayList<>();
+        populateWithInitialContacts(context);
+
+    }
+
+    private void populateWithInitialContacts(Context context)
+    {
+        //Create the Foods and add them to the list;
+
+        Contact contact3 = new Contact("ganapae@stiga.narino.gov.co");
+        mContacts.add(contact3);
+
+    }
+
+    public List<Contact> getContacts()
+    {
+        return mContacts;
+    }
+
+}
